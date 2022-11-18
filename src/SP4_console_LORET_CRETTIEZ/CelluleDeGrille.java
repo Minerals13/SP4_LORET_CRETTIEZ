ce7c2a43763c0c1c1a9efc545dc3731a175cd87d
benja <benja@DESKTOP-SR3KDU6.education.lakanal.epf.fr>
2022-11-18 13:02:29 +0100

    Classe Jeton terminée Probleme sur presenceJeton de CelluleDeGrille

--- src/SP4_console_LORET_CRETTIEZ/CelluleDeGrille.java
+++ src/SP4_console_LORET_CRETTIEZ/CelluleDeGrille.java
@@ -9,5 +9,20 @@ package SP4_console_LORET_CRETTIEZ;
  * @author benja
  */
 public class CelluleDeGrille {
-    
+   private String jetonCourant;
+   
+   public CelluleDeGrille (String jeton) {
+       jetonCourant = jeton;
+       this.jetonCourant = null;
+   }
+   
+   public boolean presenceJeton () {
+       boolean retour;
+       if ((jetonCourant == "rouge") || (jetonCourant == "jaune")) {
+       retour = true;
+       } else if (jetonCourant == null) {
+       retour = false;
+       }
+    return retour;
+   }
 }
