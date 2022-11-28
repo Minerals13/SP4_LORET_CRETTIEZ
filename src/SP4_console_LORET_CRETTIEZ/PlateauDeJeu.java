88a456eb466a45a69519bf589e2d9d7ae4c5e7b7
Emilien Loret <emilien.loret@epfedu.fr>
2022-11-28 15:43:05 +0100

    Méthode ajouterJetonDansColonne, permets de poser un jeton dans la ligne la plus basse dans une colonne choisie Méthode grilleRemplie, permets de scanner les 42 cases pour savoir si la grille est remplie (true si oui, false si non)

--- src/SP4_console_LORET_CRETTIEZ/PlateauDeJeu.java
+++ src/SP4_console_LORET_CRETTIEZ/PlateauDeJeu.java
@@ -17,14 +17,49 @@ public class PlateauDeJeu {
     
 
         for (int ligne=0;ligne<7;ligne++) {
-            for (int colonne=0;colonne<6;colonne++) {
+            for (int colonne=0;colonne<8;colonne++) {
                 
                 grille[ligne][colonne] =  new CelluleDeGrille();             
             
             }
     
         }    
-    
     }
+    int j;
+    int indLigneVide;
+    public int ajouterJetonDansColonne(Jeton, j) {  // Ajoute un jeton le plus bas possible.
+        //Jeton corresponds au jeton du joueur et int corresponds à son indice de colonne
+        for (int i=0;i<7;i++) {  //i servira d'indice de ligne pour trouver une case vide
+            if (grille[i][j]== null) { // On part de la ligne 0 (en bas) et dès qu'on trouve une ligne vide dans la colonne choisie,
+                                                // on enregistre l'indice de cette ligne dans un variable "libre"
+                indLigneVide=i; 
+                
+            } else {
+            } 
+            return indLigneVide;
+        }
+    }
+    
+    public boolean grilleRemplie() {
+    boolean remplie;    
+        for (int ligne=0;ligne<7;ligne++) {
+            for (int colonne=0;colonne<8;colonne++) {
+                if (grille[ligne][colonne]== null) {                   
+                    remplie = false;
+                    return remplie;
+                }else {
+                    remplie = true;
+                    return remplie;
+                         
+                    }
+                            
+                         
+            }
+        
+    }
+        
+
+    
 }
+    
 
