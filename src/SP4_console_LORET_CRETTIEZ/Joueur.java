f3801cf62dfa60a0921184bdc49df66a18a12dbb
benja <benja@DESKTOP-SR3KDU6.education.lakanal.epf.fr>
2022-11-28 15:12:32 +0100

    Classe Jeton terminée CelluleDeGrille terminée Joueur terminée

--- src/SP4_console_LORET_CRETTIEZ/Joueur.java
+++ src/SP4_console_LORET_CRETTIEZ/Joueur.java
@@ -4,16 +4,38 @@
  */
 package SP4_console_LORET_CRETTIEZ;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author benja
  */
 public class Joueur {
-    String Nom;
-    String Color;
+    private String Nom;
+    private String Color;
+    private ArrayList<Jeton> reserveJetons = new ArrayList();
     
-    public Joueur (String nom, String couleur, ArrayList<Jeton>) {
+    public Joueur (String nom) {
         Nom = nom;
+        int nbDesintegrateur = 0;
+    }
+    
+    public void affecterCouleur (String couleur) {
         Color = couleur;
+    }
+    
+    public int nombreDeJetons () {
+        int nbJetons = reserveJetons.size();
+        return nbJetons;
+    }
+    
+    public void ajouterJeton (Jeton jet) {
+        reserveJetons.add(jet);
+    }
+    
+    public Jeton jouerJeton () {
+        Jeton useJeton = reserveJetons.get(0);
+        reserveJetons.remove(0);
+        return useJeton;
     }
 }
