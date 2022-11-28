282135037eb66021b96dcfc8e3c6d6df216da508
benja <benja@DESKTOP-SR3KDU6.education.lakanal.epf.fr>
2022-11-28 12:47:26 +0100

    Classe Jeton terminée CelluleDeGrille en cours (il reste la méthode affecterJeton à faire)

--- src/SP4_console_LORET_CRETTIEZ/CelluleDeGrille.java
+++ src/SP4_console_LORET_CRETTIEZ/CelluleDeGrille.java
@@ -9,20 +9,53 @@ package SP4_console_LORET_CRETTIEZ;
  * @author benja
  */
 public class CelluleDeGrille {
-    private String jetonCourant;
-   
-    public CelluleDeGrille () {
-       
+    private Jeton jetonCourant;
+    public void CelluleDeGrille () {
        this.jetonCourant = null;
     }
    
     public boolean presenceJeton () {
         boolean retour;
-        if ((jetonCourant == "rouge") || (jetonCourant == "jaune")) {
+        if ((jetonCourant.lireCouleur() == "rouge") || (jetonCourant.lireCouleur() == "jaune")) {
            retour = true;
         } else {
         retour = false;
         }
     return retour;
+    }
+    
+   public void affecterJeton (Jeton jetCour) {
+       this.jetonCourant = jetCour;
    }
+   
+   
+   public String lireCouleurDuJeton (Jeton jetonCourant) {
+       if (jetonCourant != null) {
+           return jetonCourant.lireCouleur();
+       } else {
+           return "vide";
+       }
+       
+   }
+   
+   @Override
+    public String toString() {
+        String retourner;
+        if (jetonCourant.lireCouleur() == "rouge") {
+        retourner = "R";
+        } else if (jetonCourant.lireCouleur() == "jaune") {
+            retourner = "J";
+        } else {
+            retourner = ".";
+        }
+        return retourner;
+    }
+    
+    public String recupererJeton () {
+        String temp;
+        temp = jetonCourant.lireCouleur();
+        jetonCourant = null;
+        return temp;
+    }
 }
+
---
+++ src/SP4_console_LORET_CRETTIEZ/Joueur.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SP4_console_LORET_CRETTIEZ;
+
+/**
+ *
+ * @author benja
+ */
+public class Joueur {
+    String Nom;
+    String Color;
+    
+    public Joueur (String nom, String couleur, ArrayList<Jeton>) {
+        Nom = nom;
+        Color = couleur;
+    }
+}
